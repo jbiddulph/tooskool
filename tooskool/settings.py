de0984adb0df3ca1aed2783d7127f7fa2330c1c5
JOHN BIDDULPH <john.biddulph@theurbantide.com>
2024-12-29 22:14:41 +0000

    added app for static files

--- tooskool/settings.py
+++ tooskool/settings.py
@@ -160,7 +160,7 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/app/staticfiles')
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
